c48fc491a84c8b8d6180b0ac0f862038becca219
njhanne <njhanne@gmail.com>
2024-01-29 16:14:27 -0800

    lots of progress on tracking macrophages and dead cells

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.graph import pixel_graph
+from skimage import segmentation
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -8,6 +9,7 @@ from tifffile import imread
 import sys
 sys.path.append("./DirFileHelpers")
 from trackmatexml import TrackmateXML # I didn't make this!
+# https://github.com/rharkes/pyTrackMateXML
 # requires lxml and (from pip) version-parser
 
 import tkinter as tk
@@ -19,6 +21,10 @@ def find_neighbors(label_mask, connectivity = 2):
   # https://stackoverflow.com/questions/72452267/finding-identity-of-touching-labels-objects-masks-in-images-using-python
   all_pairs = defaultdict(dict)
   if len(label_mask.shape) == 2:
+    # I think I can expand the radius of the mask (i.e. what is considered 'touching') but making a 2D bool structure
+    # for connectivity. i.e. skimage.morphology.disk(4) would be a disk of radius 4
+    # no, this is not how this function works or what it does. It only finds edges that touch, not overlap
+    # I should use the overlapping code from the GM130 work.
     all_pairs[0] = find_touching_cells(label_mask, connectivity = 2)
   else:
     for slice in range(len(label_mask)):
@@ -44,11 +50,45 @@ def find_touching_cells(mask, connectivity = 2):
   pairs = defaultdict(list)
   for pair in touching_masks:
     if pair[0] != pair[1]:
-      pairs[pair[0]].append(pair[1])
+      pairs[pair[0]-1].append(pair[1]-1)
   return pairs
 
 
-# End Neighbor Helpers
+def find_distant_neighbors(label_mask, target_cells, distance):
+  # TODO figure out pixel to micron ratio in images and code that in
+  # expansion_structure = morphology.disk(distance)
+  # may need a + or - here as the labelimage and xml are 1 off
+  original_mask = np.isin(label_mask, target_cells)
+  # this 'works' in 3D but I don't think we want it to, should loop over each t slice
+  expanded_mask = segmentation.expand_labels(original_mask, distance)
+  overlapping_stain = np.logical_and(label_mask, expanded_mask)
+  overlap_cells = np.unique(label_mask[overlapping_stain])
+  matches = []
+  for cell in overlap_cells:
+    matched_cell = np.unique(expanded_mask[np.where(label_mask == cell)])
+# #   if len(matched_stain) > 1: # this prevents an error  where the entire stain overlaps with nuclei
+# #     matched_stain = matched_stain[1:]
+# #   if len(matched_stain) > 1: # this chooses the better match
+# #     dist = distance.cdist([nuc_centroids[nuc-1]]*scaling, stain_centroids[matched_stain - 1]*scaling) # calculate distances
+# #     matched_stain = matched_stain[dist.argsort()][0] # pick the match with the shortest distance
+#   matched_stain = matched_stain[0] # its a list of lists, get rid of the layering
+#
+#   # nuc_mask = np.where(nuc_img[int(nuc_centroids[nuc][2]), :, :] == (nuc), 1, 0)
+#   # stain_mask = np.where(stain_img[int(stain_centroids[matched_stain][2]), :, :] == (matched_stain), 1, 0)
+#   # test_helper(nuc_mask, stain_mask, nuc_centroids, stain_centroids, [nuc, matched_stain])
+#
+#   matches.append([nuc, matched_stain])
+# matches = np.array(matches) # matches are in image index, not centroid index
+# # Find all the duplicates and delete them
+# dups, dup_id, dup_count = np.unique(matches[:,1], return_inverse=True, return_counts=True)
+# count_mask = dup_count > 1
+# duplicates = dups[count_mask]
+# to_del = np.argwhere(np.isin(matches[:,1], duplicates))
+# first_matches = np.delete(matches, to_del, axis=0)
+
+
+
+# End neighbor helpers
 
 # xml Helpers
 def get_all_tracks(tmxml, id_range=[], duplicate_split=False, break_split=False):
@@ -63,19 +103,43 @@ def get_all_tracks(tmxml, id_range=[], duplicate_split=False, break_split=False)
   return all_tracks
 
 
-def find_cells_greater_than_prop(tmxml, apoptosis_propertyname, limit):
-
+def find_cells_greater_than_prop(tmxml, cell_propertyname, limit):
+  cells  = []
+  property_col_i = tmxml.spotheader.index(cell_propertyname)
+  cell_property = tmxml.spots[:,property_col_i]
+  cell_list = (i for i, x in enumerate(cell_property) if x >= limit)
+  for i in cell_list:
+    # gets the label id instead of the row number (i)
+    cells.append(tmxml.spots[i,0])
   return cells
 
-# End xml Helpers
+
+# End xml helpers
+
+# Analysis Helpers
+def get_frame_stats(tmxml, macrophages, apoptotic_cells, touching_cells, n_frames):
+  afs = defaultdict(dict)
+  frame_c = tmxml.spotheader.index('FRAME')
+  for frame in range(n_frames):
+    frame_cells_i = np.where(tmxml.spots[:,frame_c] == frame)[0]
+    afs[frame]['cells_label'] = tmxml.spots[frame_cells_i,0]
+    afs[frame]['macrophages'] = set(afs[frame]['cells_label']) & set(macrophages) # finds matches
+    afs[frame]['apoptotic'] = set(afs[frame]['cells_label']) & set(apoptotic_cells) # finds matches
+    afs[frame]['mac_touch'] = set(touching_cells[frame]) & set(afs[frame]['macrophages'] )
+    afs[frame]['dead_touch'] = set(touching_cells[frame]) & set(afs[frame]['apoptotic'])
+    afs[frame]['mac_dead_touch'] = set(afs[frame]['mac_touch']) & set(afs[frame]['apoptotic'])
+  return(afs)
+
+
+# End analysis helpers
 
 ### MAIN ###
 # import label mask
-image_path = filedialog.askopenfilename(title='Select directory of your images to be segmented')
+image_path = filedialog.askopenfilename(title='Select segmented labelmask')
 label_mask = imread(image_path)
 
 # import xml
-xml_track_path = filedialog.askopenfilename(title='Select directory of your images to be segmented')
+xml_track_path = filedialog.askopenfilename(title='Select trackmate xml')
 tmxml = TrackmateXML()
 tmxml.loadfile(xml_track_path)
 
@@ -84,24 +148,13 @@ touching_cells = find_neighbors(label_mask, 2)
 
 # find red cells
 # contrast in channel 1 seems to work pretty well. Should be greater than 0.05
-macrophage_propertyname = 'CONTRAST_CH1'
-limit = 0.05
-macrophages = find_cells_greater_than_prop(tmxml, macrophage_propertyname, limit)
+macrophages = find_cells_greater_than_prop(tmxml, 'MEAN_INTENSITY_CH1', 50)
+#
+# # find green cells
+# # this one is much trickier due to the bright middle for channel 2...
+# # try contrast greater SNR over 0.4
+apoptotic_cells = find_cells_greater_than_prop(tmxml, 'MAX_INTENSITY_CH2', 255)
 
-# find green cells
-# this one is much trickier due to the bright middle for channel 2...
-# try contrast greater SNR over 0.4
-apoptosis_propertyname = 'SNR_CH2'
-limit = 0.4
-apoptotic_cells = find_cells_greater_than_prop(tmxml, apoptosis_propertyname, limit)
-
-
-print(f"the tracknames:{tmxml.tracknames}")
-print(" ")
-print(f"the spotheader:{tmxml.spotheader}")
-
-# trackname = 'Track_0'
-# tracks = tmxml.analysetrack(trackname, duplicate_split=False, break_split=True)
 
 trackid_range = []
 # tracks = tmxml.analysetrackid(trackid, duplicate_split=False, break_split=True)
@@ -110,16 +163,29 @@ all_tracks = get_all_tracks(tmxml, trackid_range, duplicate_split=False, break_s
 propertyname = 'MEAN_INTENSITY_CH1'
 # property_LUT =
 # frames_LUT =
-#
+
 # intensities = []
 # frames = []
 # for tracks in all_tracks:
 #   intensities.append(tmxml.getproperty(tracks['spotids'], propertyname))
-#   # frames[tracks] = [tmxml.getproperty(track['spotids'], 'FRAME') for track in all_tracks[tracks]]
+  # frames[tracks] = [tmxml.getproperty(track['spotids'], 'FRAME') for track in all_tracks[tracks]]
+
+
+all_frame_stats = get_frame_stats(tmxml, macrophages, apoptotic_cells, touching_cells, len(label_mask))
+print('test')
+for frame in range(len(all_frame_stats)):
+  chondro_reaper_rate = (len(all_frame_stats[frame]['dead_touch']) - len(all_frame_stats[frame]['mac_dead_touch'])) / \
+                            (len(all_frame_stats[frame]['cells_label']) - len(all_frame_stats[frame]['apoptotic']) - len(all_frame_stats[frame]['macrophages']))
+  macro_reaper_rate = (len(all_frame_stats[frame]['mac_dead_touch'])) / (len(all_frame_stats[frame]['macrophages']))
+  print('\n frame ', frame)
+  print('chondro death touch: ', chondro_reaper_rate * 100)
+  print('macro death touch: ', macro_reaper_rate * 100)
+  print('macro effectiveness: ', macro_reaper_rate / chondro_reaper_rate)
+
 
 for track in all_tracks:
   for sub_track in all_tracks[track]:
-    plt.plot(tmxml.getproperty(sub_track['spotids'], 'FRAME'), tmxml.getproperty(sub_track['spotids'], propertyname)) #, label='Cell ' + str(tracks[i]['cell']) + ' ; Child of '+str(tracks[i]['parent']))
+    plt.plot(tmxml.getproperty(sub_track.spotids, 'FRAME'), tmxml.getproperty(sub_track.spotids, propertyname)) #, label='Cell ' + str(tracks[i]['cell']) + ' ; Child of '+str(tracks[i]['parent']))
 plt.xlabel('frame')
 plt.ylabel(propertyname)
 plt.legend()
@@ -127,14 +193,7 @@ plt.legend()
 
 
 
-# fig, axs = plt.subplots(len(frames),1)
-# fig.set_size_inches(5,15)
-# for i in range(len(frames)):
-#     axs[i].plot(frames[i], intensities[i], label='Cell ' + str(tracks[i]['cell']) + ' ; Child of '+str(tracks[i]['parent']))
-#     axs[i].set_xlabel('frame')
-#     axs[i].set_ylabel(propertyname)
-#     axs[i].legend()
-#     # axs[i].set_title(trackname)
+
 
 
 
--- trackmatexml/TrackmateXML.py
+++ trackmatexml/TrackmateXML.py
@@ -14,13 +14,52 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+import io
 import json
 import os
 import logging
-
+import sys
+from contextlib import AbstractContextManager
+from pathlib import Path
+from types import TracebackType
+from typing import Any, Union, Optional, Type, BinaryIO, List, Dict
 import numpy as np
 from lxml import etree
-from version_parser import Version, VersionType
+from dataclasses import dataclass
+
+
+@dataclass
+class AnalyzedTrack:
+    parent: int
+    cell: int
+    spotids: np.ndarray[Any, np.dtype[np.int32]]
+    track: bool
+
+
+class TrackmateXMLFile(AbstractContextManager[Any]):
+    """
+    Context manager for handeling TrackmateXML files
+    """
+
+    def __init__(self, pth: Union[str, os.PathLike[Any]]) -> None:
+        pth = Path(pth)
+        if not pth.exists() or not pth.is_file():
+            raise FileNotFoundError(pth)
+        self.pth = pth
+
+    def __enter__(self) -> "TrackmateXML":
+        self.tmxml = TrackmateXML()
+        self.file = io.FileIO(self.pth, "r")
+        self.tmxml.loadstream(self.file)
+        return self.tmxml
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        self.file.close()
 
 
 class TrackmateXML:
@@ -34,76 +73,106 @@ class TrackmateXML:
     https://scholar.google.com/scholar?cluster=9846627681021220605
     """
 
-    def __init__(self):
-        self.version = None
-        self.spatialunits = None
-        self.timeunits = None
-        self.spotheader = []  # type:[str]
+    def __init__(self, loglevel: int = logging.ERROR) -> None:
+        self.logger = logging.getLogger(__name__)
+        if loglevel < logging.ERROR:
+            handler = logging.StreamHandler(
+                sys.stdout
+            )  # set output to stdout instead of stderror
+            handler.setLevel(loglevel)
+            formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+        else:
+            self.logger.setLevel(loglevel)
+        self.logger.info("Logger Initialized")
+        self.version = ""  # type: str
+        self.spatialunits = ""  # type: str
+        self.timeunits = ""  # type: str
+        self.spotheader = []  # type:List[str]
         self.spots = np.zeros((0, 0), dtype=float)
-        self.tracks = []  # type: [np.ndarray]
-        self.tracknames = []  # type: [str]
-        self.displaysettings = {}
+        self.tracks = []  # type: List[np.ndarray[Any, np.dtype[np.int32]]]
+        self.tracknames = []  # type: List[str]
+        self.displaysettings = {}  # type: Dict[str, str]
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         if self.version:
             return True
         else:
             return False
 
-    def loadfile(self, pth: os.PathLike) -> None:
+    def loadstream(self, fp: BinaryIO) -> None:
+        self._load(etree.parse(fp))
+
+    def loadfile(self, pth: Union[str, os.PathLike[Any]]) -> None:
         """
         Load a TrackMate XML-file
         """
         self._load(etree.parse(pth))
 
-    def loadtree(self, tree: etree.ElementTree) -> None:
+    def loadtree(self, tree: etree._ElementTree) -> None:
         """
         Load a Trackmate XML-tree
         """
         self._load(tree)
 
     def gettraces(
-        self, trackname, spot_property: str, duplicate_split=False, break_split=False
-    ) -> list:
+        self,
+        trackname: str,
+        spot_property: str,
+        duplicate_split: bool = False,
+        break_split: bool = False,
+    ) -> List[np.ndarray[Any, np.dtype[np.float64]]]:
         """
         Get traces from a trackname and a spot-property
         """
         tracks = self.analysetrack(trackname, duplicate_split, break_split)
-        return [self.getproperty(track["spotids"], spot_property) for track in tracks]
+        return [self.getproperty(track.spotids, spot_property) for track in tracks]
 
-    def getproperty(self, spotids: np.ndarray, spot_property: str) -> np.ndarray:
+    def getproperty(
+        self, spotids: np.ndarray[Any, np.dtype[np.int32]], spot_property: str
+    ) -> np.ndarray[Any, np.dtype[np.float64]]:
         """
         Get properties from spotids
         """
         if spot_property not in self.spotheader:
-            logging.error(f"{spot_property} not in spot properties")
+            self.logger.error(f"{spot_property} not in spot properties")
             return np.zeros((0, 0), dtype=float)
         prop_idx = self.spotheader.index(spot_property)
         spotid_idx = self.spotheader.index("ID")
-        res = np.zeros(len(spotids), dtype=float)
+        res = np.zeros(len(spotids), dtype=np.float64)
         for i, s in enumerate(spotids):
-            res[i] = self.spots[self.spots[:, spotid_idx] == s, prop_idx]
+            res[i] = self.spots[self.spots[:, spotid_idx] == s, prop_idx][0]
         return res
 
-    def _load(self, tree) -> None:
+    def _load(self, tree: etree._ElementTree) -> None:
+        self.logger.info("Getting version")
         self._getversion(tree.getroot())
+        self.logger.info("Analyzing tree")
         self._analysetree(tree)
+        self.logger.info("Finished loading")
 
     def getversion(self) -> str:
         """
         Get the version of the TrackmateXML data as string.
         """
-        return self.version.get_typed_version(VersionType.VERSION)
+        return self.version if self.version else ""
 
-    def _getversion(self, root) -> None:
+    def _getversion(self, root: etree._Element) -> None:
         if root.tag == "TrackMate":
-            self.version = Version(root.attrib.get("version", None))
-            if self.version is None:
-                logging.error(f"Invalid Version")
+            v = root.attrib.get("version", "")
+            if v == "":
+                self.logger.error(f"Invalid Version")
+            elif type(v) == bytes:
+                self.logger.error(f"Invalid Version")
+            else:
+                self.version = str(v)
         else:
-            logging.error("Not a TrackMateXML")
+            self.logger.error("Not a TrackMateXML")
 
-    def _analysetree(self, tree) -> None:
+    def _analysetree(self, tree: etree._ElementTree) -> None:
         root = tree.getroot()
         for element in root:
             if element.tag == "Log":
@@ -117,17 +186,17 @@ class TrackmateXML:
             elif element.tag == "DisplaySettings":
                 self._get_display_settings(element)
             else:
-                logging.error(f"Unrecognised element {element}")
+                self.logger.error(f"Unrecognised element {element}")
 
-    def _getlog(self, element: etree.Element) -> None:
+    def _getlog(self, element: etree._Element) -> None:
         self.log = element.text
 
-    def _get_display_settings(self, element: etree.Element) -> None:
-        self.displaysettings = json.loads(element.text)
+    def _get_display_settings(self, element: etree._Element) -> None:
+        self.displaysettings = json.loads(str(element.text))
 
-    def _getmodel(self, element: etree.Element) -> None:
-        self.spatialunits = element.attrib.get("spatialunits", None)
-        self.timeunits = element.attrib.get("timeunits", None)
+    def _getmodel(self, element: etree._Element) -> None:
+        self.spatialunits = str(element.attrib.get("spatialunits", ""))
+        self.timeunits = str(element.attrib.get("timeunits", ""))
         for subelement in element:
             if subelement.tag == "FeatureDeclarations":
                 pass  # would be nice if the feature declaration would actually list the features in the xml, but instead it lists all possible features
@@ -138,64 +207,64 @@ class TrackmateXML:
             elif subelement.tag == "FilteredTracks":
                 self._getfilteredtracks(subelement)
             else:
-                logging.error(f"Unrecognised element {element}")
+                self.logger.error(f"Unrecognised element {element}")
 
-    def _getsettings(self, element: etree.Element) -> None:
+    def _getsettings(self, element: etree._Element) -> None:
         """
         Could be added, but is not required for displaying intensity tracks
         """
         pass
 
-    def _getfilteredtracks(self, element: etree.Element) -> None:
+    def _getfilteredtracks(self, element: etree._Element) -> None:
         """
         Could be added, but is not required for displaying intensity tracks
         """
         pass
 
-    def _get_gui_state(self, element: etree.Element) -> None:
+    def _get_gui_state(self, element: etree._Element) -> None:
         """
         Could be added, but does not seem usefull in python.
         """
         pass
 
-    def _getspots(self, element: etree.Element) -> None:
+    def _getspots(self, element: etree._Element) -> None:
         """
         Put all numeric spot data in a numpy array.
         """
-        nspots = int(element.attrib.get("nspots", "0"))
+        nspots = int(str(element.attrib.get("nspots", "0")))
         # construct header
         spotid = 0
         spot = element[0][0]
-        keys = [a for a in spot.attrib]
+        keys = [str(a) for a in spot.attrib]
         for k in keys:
             try:
                 float(spot.attrib[k])
             except ValueError:  # remove keys we cannot convert to floats
                 keys.remove(k)
         self.spotheader = keys
-        self.spots = np.zeros((nspots, len(keys)))
+        self.spots = np.zeros((nspots, len(keys)), dtype=np.float64)
         for sif in element:
             for spot in sif:
                 for i, k in enumerate(keys):
-                    self.spots[spotid, i] = spot.attrib.get(k, None)
+                    self.spots[spotid, i] = float(str(spot.attrib.get(k, "nan")))
                 spotid += 1
 
-    def _gettracks(self, element: etree.Element) -> None:
+    def _gettracks(self, element: etree._Element) -> None:
         """
         Importing only source and target into a numpy array and listing the trackname.
         We could import more, but it is not needed for displaying intensity tracks.
         """
         for track in element:
-            t = np.zeros((len(track), 2), dtype=int)
+            t = np.zeros((len(track), 2), dtype=np.int32)
             for i, edge in enumerate(track):
-                t[i, 0] = edge.attrib.get("SPOT_SOURCE_ID", -1)
-                t[i, 1] = edge.attrib.get("SPOT_TARGET_ID", -1)
+                t[i, 0] = int(str(edge.attrib.get("SPOT_SOURCE_ID", "-1")))
+                t[i, 1] = int(str(edge.attrib.get("SPOT_TARGET_ID", "-1")))
             self.tracks.append(t)
-            self.tracknames.append(track.attrib.get("name", "unknown"))
+            self.tracknames.append(str(track.attrib.get("name", "unknown")))
 
     def analysetrack(
-        self, trackname: str, duplicate_split=False, break_split=False
-    ) -> list:
+        self, trackname: str, duplicate_split: bool = False, break_split: bool = False
+    ) -> List[AnalyzedTrack]:
         """
         Traces a track to find the sequence of spotids
         """
@@ -203,86 +272,84 @@ class TrackmateXML:
         return self.analysetrackid(trackid, duplicate_split, break_split)
 
     def analysetrackid(
-        self, trackid: int, duplicate_split=False, break_split=False
-    ) -> list:
+        self, trackid: int, duplicate_split: bool = False, break_split: bool = False
+    ) -> List[AnalyzedTrack]:
         """
         Traces a track to find the sequence of spotids
         """
         track = self.tracks[trackid]
         unique_sources = np.setdiff1d(track[:, 0], track[:, 1], assume_unique=True)
         if unique_sources.size != 1:
-            logging.error(
+            self.logger.error(
                 f"Track has {unique_sources.size} startingpoints. Cannot follow track."
             )
             return []
         cellid = 1
         traced_tracks = [
-            {
-                "parent": 0,
-                "cell": cellid,
-                "spotids": track[
-                    np.argwhere(track[:, 0] == unique_sources), :
-                ].flatten(),
-                "track": True,
-            }
+            AnalyzedTrack(
+                parent=0,
+                cell=cellid,
+                spotids=track[np.argwhere(track[:, 0] == unique_sources), :].flatten(),
+                track=True,
+            )
         ]
-        while any([x["track"] for x in traced_tracks]):
+        while any([x.track for x in traced_tracks]):
             for traced_track in traced_tracks:
-                if not traced_track["track"]:
+                if not traced_track.track:
                     continue
-                traced_track_ids = traced_track["spotids"]
+                traced_track_ids = traced_track.spotids
                 targetidx = np.argwhere(
                     track[:, 0] == traced_track_ids[-1]
                 ).flatten()  # do we find the last index in the sources?
                 if targetidx.size == 0:
-                    traced_track["track"] = False  # reached the end of the track
+                    traced_track.track = False  # reached the end of the track
                 elif targetidx.size == 1:
-                    traced_track["spotids"] = np.concatenate(
+                    traced_track.spotids = np.concatenate(
                         (traced_track_ids, track[targetidx, 1])
                     )  # append target to track
                 else:  # multiple targets, a split
                     if duplicate_split:  # a copy of the history is added to each track
                         for i in range(1, targetidx.size):
                             spotids = np.concatenate(
-                                (traced_track_ids, [track[targetidx[i], 1]])
+                                (traced_track_ids, track[targetidx[i], 1].flatten())
                             )
                             cellid += 1
-                            new_track = {
-                                "parent": traced_track["cell"],
-                                "cell": cellid,
-                                "spotids": spotids,
-                                "track": True,
-                            }
+                            new_track = AnalyzedTrack(
+                                parent=traced_track.cell,
+                                cell=cellid,
+                                spotids=spotids,
+                                track=True,
+                            )
                             traced_tracks.append(new_track)
                     else:
                         for i in range(1, targetidx.size):
                             spotids = track[targetidx[i], :]
                             cellid += 1
-                            new_track = {
-                                "parent": traced_track["cell"],
-                                "cell": cellid,
-                                "spotids": spotids,
-                                "track": True,
-                            }
+                            new_track = AnalyzedTrack(
+                                parent=traced_track.cell,
+                                cell=cellid,
+                                spotids=spotids,
+                                track=True,
+                            )
                             traced_tracks.append(new_track)
                     if break_split:
-                        traced_track["track"] = False  # end the parent
+                        traced_track.track = False  # end the parent
                         if duplicate_split:
                             spotids = np.concatenate(
-                                (traced_track_ids, [track[targetidx[0], 1]])
+                                (traced_track_ids, track[targetidx[0], 1].flatten())
                             )
                         else:
                             spotids = track[targetidx[0], :]  # start child
                         cellid += 1
-                        new_track = {
-                            "parent": traced_track["cell"],
-                            "cell": cellid,
-                            "spotids": spotids,
-                            "track": True,
-                        }
+                        new_track = AnalyzedTrack(
+                            parent=traced_track.cell,
+                            cell=cellid,
+                            spotids=spotids,
+                            track=True,
+                        )
                         traced_tracks.append(new_track)
                     else:
-                        traced_track["spotids"] = np.concatenate(
-                            (traced_track_ids, [track[targetidx[0], 1]])
+                        traced_track.spotids = np.concatenate(
+                            (traced_track_ids, track[targetidx[0], 1].flatten())
                         )  # append target to track
         return traced_tracks
