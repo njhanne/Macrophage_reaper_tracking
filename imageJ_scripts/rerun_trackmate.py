f16bb810ecb793f7b4f719ccf5bc54cd33cb0f23
nicholas <njhanne@gmail.com>
2024-05-07 14:02:48 -0700

    made script to rerun trackmate tracking without redoing cellpose detection

--- imageJ_scripts/rerun_trackmate.py
+++ imageJ_scripts/rerun_trackmate.py
@@ -1,6 +1,7 @@
 # https://imagej.net/plugins/trackmate/scripting/trackmate-detectors-trackers-keys
 # https://github.com/trackmate-sc/TrackMate/blob/master/scripts/ExampleScript_1.py
 # https://github.com/trackmate-sc/TrackMate/blob/master/scripts/TrackMate-Cellpose-script.py
+# https://github.com/trackmate-sc/TrackMate/blob/master/scripts/ExampleScript_ReadTrackMateFile.py
 
 import sys
 from datetime import datetime as dt
@@ -23,12 +24,15 @@ import fiji.plugin.trackmate.features.FeatureFilter as FeatureFilter
 
 from fiji.plugin.trackmate.util import LogRecorder
 from fiji.plugin.trackmate.io import TmXmlWriter
+from fiji.plugin.trackmate.io import TmXmlReader
 from fiji.plugin.trackmate.util import TMUtils
 from fiji.plugin.trackmate import SelectionModel
 from fiji.plugin.trackmate.cellpose import CellposeDetectorFactory
 import fiji.plugin.trackmate.features.FeatureFilter as FeatureFilter
 from fiji.plugin.trackmate.cellpose.CellposeSettings import PretrainedModel
 from fiji.plugin.trackmate.action import LabelImgExporter
+
+from java.io import File
 
 # We have to do the following to avoid errors with UTF8 chars generated in
 # TrackMate that will mess with our Fiji Jython.
@@ -61,17 +65,16 @@ channel_to_process = 3
 
 # Image files to analyse.
 directory_to_process = os.path.normpath("C:/Users/njhan/Box/macrophage_coculture/processed/stabilized_tiffs")
-file_paths = find_all_filepaths(directory_to_process, '.tiff')
+file_paths = find_all_filepaths(directory_to_process, '.xml')
 print(file_paths)
 
 
 # -------------------------------------------------------
 # 	ACTUAL CODE.
 # ------------------------------------------------------
-def run(image_file):
-  # Open image.
-  imp = IJ.openImage(image_file)
-  cal = imp.getCalibration()
+def run(xml_file):
+  # We have to feed a logger to the reader.
+  logger = Logger.IJ_LOGGER
 
   # Logger -> content will be saved in the XML file.
   logger = LogRecorder(Logger.VOID_LOGGER)
@@ -81,12 +84,108 @@ def run(image_file):
   dt_string = dt.now().strftime("%d/%m/%Y %H:%M:%S")
   logger.log(dt_string + '\n\n')
 
+  # -------------------
+  # Instantiate reader
+  # -------------------
+  xml_file = File(xml_file)
+  reader = TmXmlReader(xml_file)
+  if not reader.isReadingOk():
+    sys.exit(reader.getErrorMessage())
+
+  # -----------------
+  # Get a full model
+  # -----------------
+
+  # This will return a fully working model, with everything
+  # stored in the file. Missing fields (e.g. tracks) will be
+  # null or None in python
+  model = reader.getModel()
+  # model is a fiji.plugin.trackmate.Model
+
+  # ----------------
+  # Display results
+  # ----------------
+
+  # We can now plainly display the model. It will be shown on an
+  # empty image with default magnification because we do not
+  # specify an image to display it. We will see later how to
+  # retrieve the image on which the data was generated.
+
+  # A selection.
+  sm = SelectionModel(model)
+
+  # Read the display settings that was saved in the file.
+  ds = reader.getDisplaySettings()
+
+  # The viewer.
+  # displayer = HyperStackDisplayer(model, sm, ds)
+  # displayer.render()
+
+  # ---------------------------------------------
+  # Get only part of the data stored in the file
+  # ---------------------------------------------
+
+  # You might want to access only separate parts of the
+  # model.
+
+  # spots = model.getSpots()
+  # spots is a fiji.plugin.trackmate.SpotCollection
+
+  # logger.log(str(spots))
+
+  # If you want to get the tracks, it is a bit trickier.
+  # Internally, the tracks are stored as a huge mathematical
+  # simple graph, which is what you retrieve from the file.
+  # There are methods to rebuild the actual tracks, taking
+  # into account for everything, but frankly, if you want to
+  # do that it is simpler to go through the model:
+
+  # trackIDs = model.getTrackModel().trackIDs(True)  # only filtered out ones
+  # for id in trackIDs:
+  #   logger.log(str(id) + ' - ' + str(model.getTrackModel().trackEdges(id)))
+
+  # ---------------------------------------
+  # Building a settings object from a file
+  # ---------------------------------------
+
+  # The settings object can be  used to regenerate the full
+  # model from scratch. It contains all the parameters that
+  # were configured when the GUI was used and the source image.
+
+  # First load the image. TrackMate will retrieve the image *PATH*
+  # from the XML file and try to reopen it. It works properly only
+  # if the image was saved as an ImageJ TIFF.
+  imp = reader.readImage()
+  # Reading the image does not display it.
+
+  # Now we can read the settings objects. Notice the method
+  # asks for the image. This is the settings object will be built
+  # with a link to the source image.
+  settings = reader.readSettings(imp)
+
+  # Let's print the settings object.
+  logger.log(str('\n\nSETTINGS:'))
+  logger.log(str(settings))
+
+  # Display the image now.
+  # imp.show()
+
+  # With this, we can overlay the model and the source image:
+  # displayer = HyperStackDisplayer(model, sm, imp, ds)
+  # displayer.render()
+
+
+  # here is the old code to run trackmate
+  cal = imp.getCalibration()
+
+
+
   # ------------------------
   # Prepare settings object
   # ------------------------
 
   settings = Settings(imp)
-  setup = settings.toStringImageInfo()
+  # setup = settings.toStringImageInfo()
 
   # Configure Cellpose default detector.
 
@@ -141,7 +240,7 @@ def run(image_file):
   # Instantiate plugin
   # -------------------
 
-  trackmate = TrackMate(settings)
+  trackmate = TrackMate(model, settings)
   trackmate.computeSpotFeatures(True)
   trackmate.computeTrackFeatures(True)
   trackmate.getModel().setLogger(logger)
@@ -155,7 +254,8 @@ def run(image_file):
     print(str(trackmate.getErrorMessage()))
     return
 
-  ok = trackmate.process()
+  # ok = trackmate.process()
+  ok = trackmate.execTracking()
   if not ok:
     print(str(trackmate.getErrorMessage()))
     return
@@ -173,11 +273,11 @@ def run(image_file):
   writer.writeToFile();
   print("Results saved to: " + saveFile.toString() + '\n');
 
-  lblImg = LabelImgExporter()
-  lblImg.createLabelImagePlus(trackmate, False, False, True, logger).show()
-  c = WindowManager.getCurrentImage()
-  IJ.save(c, "C:/Users/njhan/Box/macrophage_coculture/processed/label_images/" + c.getTitle())
-  c.close()
+  # lblImg = LabelImgExporter()
+  # lblImg.createLabelImagePlus(trackmate, False, False, True, logger).show()
+  # c = WindowManager.getCurrentImage()
+  # IJ.save(c, "C:/Users/njhan/Box/macrophage_coculture/processed/label_images/" + c.getTitle())
+  # c.close()
 
   # ----------------
   # Display results
