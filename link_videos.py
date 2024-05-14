b189602e02cd207016f891f7fbbdb91ccaf50152
nicholas <njhanne@gmail.com>
2024-05-13 19:14:48 -0700

    finalizing video-linking code. Will commit 'main.py' later, it's still being edited

--- link_videos.py
+++ link_videos.py
@@ -31,8 +31,52 @@ def get_all_tracks(tmxml, id_range=[], duplicate_split=True, break_split=True):
   return all_tracks
 
 
-# End XML Helpers
+def get_tmxml_column(tmxml, names):
+  name_list = []
+  for name in names:
+    name_list.append(tmxml.spotheader.index(name))
+  return name_list
 
+
+def link_tracks(linked_df, tmxml_link):
+  tracks = get_all_tracks(tmxml_link)
+  cell_tracks = []
+  for track in tracks:
+    for cell in tracks[track]:
+      cell_tracks.append(cell.spotids)
+  cell_tracks = pd.DataFrame(cell_tracks)
+  cell_tracks = pd.merge(linked_df, cell_tracks, how='left', left_on=['track_spots'], right_on=[0])
+  cell_tracks.columns = ['og_spots', 'track_spots', 'frame', 'track_spots2', 'linked_track_spot']
+  cell_tracks = pd.merge(linked_df, cell_tracks, how='left', left_on=['track_spots'], right_on=['linked_track_spot'])
+  cell_tracks = cell_tracks.iloc[:,[3,0]].dropna()
+  cell_tracks.columns = ['24hr_spots', '48hr_spots']
+  return cell_tracks
+
+
+def get_video_links(tmxml_link, tmxml_24, tmxml_48):
+  col_names = ['FRAME', 'ID', 'POSITION_X', 'POSITION_Y']
+  slice_indices = get_tmxml_column(tmxml_24, col_names)
+  first_centroid_list = pd.DataFrame(tmxml_24.spots[tmxml_24.spots[:,slice_indices[0]] == 177][:,slice_indices[1:]])
+  slice_indices = get_tmxml_column(tmxml_48, col_names)
+  second_centroid_list = pd.DataFrame(tmxml_48.spots[tmxml_48.spots[:, slice_indices[0]] == 0][:, slice_indices[1:]])
+  slice_indices = get_tmxml_column(tmxml_link, col_names)
+  first_link_centroid_list = pd.DataFrame(tmxml_link.spots[tmxml_link.spots[:, slice_indices[0]] == 0][:, slice_indices[1:]])
+  second_link_centroid_list = pd.DataFrame(tmxml_link.spots[tmxml_link.spots[:, slice_indices[0]] == 1][:, slice_indices[1:]])
+
+
+  first_linked_df = pd.merge(first_centroid_list, first_link_centroid_list, how='left', left_on = [1,2], right_on = [1,2])
+  # this is kinda hacky, but works since there is no 'x' movement, only 'y'
+  # very unlikely that two cells share exactly the same x centroid. It is possible! A bridge to cross if that occurs...
+  second_linked_df = pd.merge(second_centroid_list, second_link_centroid_list, how='left', left_on=[1], right_on=[1])
+  linked_df = first_linked_df.iloc[:,[0,3]].assign(frame=0)
+  linked_df = pd.concat([linked_df, second_linked_df.iloc[:,[0,3]].assign(frame=1)])
+  linked_df.columns = ['og_spots', 'track_spots', 'frame']
+
+  linked_tracks = link_tracks(linked_df, tmxml_link)
+  return linked_tracks
+
+
+# End XML Helpers
 
 ### Main ###
 # The time-series images are named 24 and 48 hr. The number in the image name correspond to the same sample/well
@@ -49,7 +93,6 @@ data_dir = (Path.cwd() / 'data').resolve()
 process_dir = (data_dir / 'processed' / 'label_images').resolve()
 processed_dir = (data_dir / 'processed' / 'stabilized_tiffs').resolve()
 results_dir = (data_dir / 'results' / 'tracks_csv').resolve()
-
 
 image_dirs, image_paths = find_all_filepaths(Path(process_dir), '.tiff')
 link_xml_dirs, link_xml_paths = find_all_filepaths(process_dir, '.xml')
@@ -76,7 +119,7 @@ for sample_name in sample_info['new_filename'].unique():
       lmi_path = [lp for lp in image_paths if lp.parts[-1].startswith(this_sample_info['new_filename_timeless'].values[0]+'_link')]
 
       if len(lmi_path) == 0: # create the link mask file
-        print('creating link mask file, you will need to run trackmate script in imagej to create the links!')
+        print('creating link mask file for '+ this_sample_info['new_filename_timeless'].values[0] +', you will need to run trackmate script in imagej to create the links!')
         lbl_24 = [lp for lp in image_paths if lp.parts[-1].startswith('LblImg_' + this_sample_info['new_filename'].values[0]+'_')]
         lbl_48 = [lp for lp in image_paths if lp.parts[-1].startswith('LblImg_' + paired_sample['new_filename'].values[0]+'_')]
 
@@ -90,15 +133,30 @@ for sample_name in sample_info['new_filename'].unique():
         imwrite(savename, combined_mask.astype('float32'), imagej=True, metadata={'axes': 'TYX'}, )
 
       if len(xml_path) != 0 and len(lmi_path) != 0: # link two existing csv with linking xml
-        print('Analyzing ' + sample_info['new_filename_timeless'].values[0])
+        print('Creating link csv for ' + this_sample_info['new_filename_timeless'].values[0])
         tmxml_path = [tm for tm in full_xml_paths if tm.parts[-1].startswith(this_sample_info['new_filename'].values[0]+'_')]
         tmxml_24 = TrackmateXML()
-        tmxml_24.loadfile(tmxml_path[0])  # import TrackMate XML
+        try:
+          tmxml_24.loadfile(tmxml_path[0])  # import TrackMate XML
+          loaded= True
+        except:
+          loaded= False
+          print('could not load xml for ' + this_sample_info['new_filename'].values[0])
 
-        tmxml_path = [tm for tm in full_xml_paths if tm.parts[-1].startswith(paired_sample['new_filename'].values[0]+'_')]
-        tmxml_48 = TrackmateXML()
-        tmxml_48.loadfile(tmxml_path[0])
+        if loaded:
+          tmxml_path = [tm for tm in full_xml_paths if tm.parts[-1].startswith(paired_sample['new_filename'].values[0] + '_')]
+          tmxml_48 = TrackmateXML()
+          try:
+            tmxml_48.loadfile(tmxml_path[0])
+          except:
+            loaded = False
+            print('could not load xml for ' + paired_sample['new_filename'].values[0])
+        if loaded:
+            tmxml_path = [tm for tm in link_xml_paths if
+                          tm.parts[-1].startswith(paired_sample['new_filename_timeless'].values[0] + '_link')]
+            tmxml_link = TrackmateXML()
+            tmxml_link.loadfile(tmxml_path[0])
 
-
-
-
+            links_df = get_video_links(tmxml_link, tmxml_24, tmxml_48)
+            save_name = (processed_dir / (this_sample_info['new_filename_timeless'].values[0] + '_links.csv')).resolve()
+            links_df.to_csv(save_name)
